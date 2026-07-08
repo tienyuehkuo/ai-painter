2600ecd1ed5e2ca9c2544d5fdc2cd1c5a75b05ca
DianYueGuo <dianyueguo@gmail.com>
2026-07-08 15:01:16 +0800

    Add pixels

--- canvas.py
+++ canvas.py
@@ -2,6 +2,10 @@ class Canvas:
     def __init__(self, width, height):
         self._width = width
         self._height = height
+        self._pixels = [
+            [(255, 255, 255) for x in range(width)]
+            for y in range(height)
+        ]
 
     @property
     def width(self):
@@ -10,3 +14,7 @@ class Canvas:
     @property
     def height(self):
         return self._height
+
+    @property
+    def pixels(self):
+        return self._pixels
