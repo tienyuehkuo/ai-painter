32ff91f6572bac2ec9156ec4eebb3a312f4911ef
DianYueGuo <dianyueguo@gmail.com>
2026-07-08 14:53:09 +0800

    Create Canvas class

---
+++ canvas.py
@@ -0,0 +1,12 @@
+class Canvas:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
